663fd67b403c5f7b98b2e3cec215cdb2c6aff796
Benjamin Bailon <benjamin.bailon@outlook.com>
2024-08-05 19:21:01 -0600

    chore: Remove unused Income and Outcome doctypes and their files

--- cashflow/cashflow/doctype/outcome/outcome.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Benjamin Bailon and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Outcome(Document):
-	pass
